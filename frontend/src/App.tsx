513f09337bfe495ddae2e48e07797ef21ea4f71f
Matheus N. Borges <matheusnborgesdev@gmail.com>
2022-11-16 10:27:57 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton></NotificationButton>
+    </>
   )
 }
 
