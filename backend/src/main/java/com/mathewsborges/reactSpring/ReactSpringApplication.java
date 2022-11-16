72f4ebea7d263a82b76927b1e27d043f99b412cd
Matheus N. Borges <matheusnborgesdev@gmail.com>
2022-11-16 09:29:27 -0300

    Project created

---
+++ backend/src/main/java/com/mathewsborges/reactSpring/ReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mathewsborges.reactSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mathewsborges/reactSpring/ReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathewsborges.reactSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
