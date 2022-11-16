febefcbfb7488fb972c3cb92d7b8b046616a3e21
Matheus N. Borges <matheusnborgesdev@gmail.com>
2022-11-16 11:22:50 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
@@ -22,4 +21,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
