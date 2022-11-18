9dfaedbe15430940d1b1f75208a6dddb522a2ac8
Matheus N. Borges <matheusnborgesdev@gmail.com>
2022-11-18 14:56:07 -0300

    Date Select

--- backend/src/main/java/com/mathewsborges/reactSpring/repositories/SaleRepository.java
+++ backend/src/main/java/com/mathewsborges/reactSpring/repositories/SaleRepository.java
@@ -1,14 +1,19 @@
 package com.mathewsborges.reactSpring.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mathewsborges.reactSpring.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 	
 	
-	
-	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 	
 	
 	
