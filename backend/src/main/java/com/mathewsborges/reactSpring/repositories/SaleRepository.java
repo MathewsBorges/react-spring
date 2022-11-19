0b50801c46497d8259ac7fda302ae13d5ded1a2d
Matheus N. Borges <matheusnborgesdev@gmail.com>
2022-11-19 10:58:15 -0300

    New

--- backend/src/main/java/com/mathewsborges/reactSpring/repositories/SaleRepository.java
+++ backend/src/main/java/com/mathewsborges/reactSpring/repositories/SaleRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+
 import com.mathewsborges.reactSpring.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,10 @@ import './styles.css';
 import NotificationButton from '../NotificationButton';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
+import { Sale } from '../../models/sale';
 
 function SalesCard() {
 
@@ -12,8 +15,16 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]);
 
 
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales`)
+            .then(response => {
+                setSales(response.data.content)
+
+            })
+    }, []);
 
 
 
@@ -56,6 +67,25 @@ function SalesCard() {
                         </tr>
                     </thead>
                     <tbody>
+                        {
+                            sales.map(sale => {
+                                return (
+                                    <tr key={sale.id}>
+                                        <td className="show992">#{sale.id}</td>
+                                        <td className="show576">#{sale.date}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>R$ {sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="dsmeta-red-btn-container">
+                                                <NotificationButton />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
                         <tr>
                             <td className="show992">#341</td>
                             <td className="show576">08/07/2022</td>
